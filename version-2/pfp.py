b3bae40d8889cfbcef4057081d184604db7fa356
j2nx <j@heal.rip>
2023-08-26 22:46:55 -0400

    Create pfp.py

---
+++ version-2/pfp.py
@@ -0,0 +1,5 @@
+class pfps:
+pfps = [ "https://cdn.discordapp.com/attachments/1135674543990652928/1142324434850562048/IMG_7427.jpg"
+"https://cdn.discordapp.com/attachments/1135674543990652928/1142324436041744504/IMG_2851.jpg"
+"https://cdn.discordapp.com/attachments/1135674543990652928/1142324434401775686/3f76734df2e6dabe70f54c1a10e5fca9.gif"
+         ]
